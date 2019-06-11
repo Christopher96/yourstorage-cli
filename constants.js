f36eba22e83c68eae0d7ab2dae4ceecc2c455635
Christopher Gauffin <christopher.gauffin@gmail.com>
2019-06-12 00:42:57 +0200

    first commit

---
+++ constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+    commands: {
+        LIST_USERS: 'List users',
+        CONNECT: 'Connect',
+        CHAT: 'Chat',
+        BROWSE: 'Browse files',
+        HOME: 'Leave'
+    }
+}
