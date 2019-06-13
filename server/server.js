15711b0a50d74b68c1c60fafe0509203b1264507
Christopher Gauffin <christopher.gauffin@gmail.com>
2019-06-13 18:17:24 +0200

    rm .git server

---
+++ server/server.js
@@ -0,0 +1,91 @@
+const constants = require('../constants')
+const commands = constants.commands
+const events = constants.events
+
+const port = process.env.PORT || 3000
+
+// const express = require('express')
+// const app = express()
+//
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//
+// const server = require('http').createServer(app)
+const io = require('socket.io')(port)
+
+io.on('connection', (socket) => {
+    console.log("Client detected [%s]", socket.id)
+
+    socket.on(events.MESSAGE, function(data) {
+        console.log("[%s] -> [%s] Message: %s", socket.id, data.targetUser, data.message);
+        socket.to(data.targetUser).emit(events.MESSAGE_RECEIVED, {
+            fromUser: socket.id,
+            message: data.message
+        })
+    })
+
+    socket.on(events.COMMAND, function(data) {
+        console.log('[%s] Command: %s', socket.id, data.command)
+
+        let response = false
+
+        switch(data.command) {
+            case commands.LIST_USERS:
+                response = fetchUsers()
+                break;
+            case commands.CONNECT:
+                const users = fetchUsers()
+                if(users.includes(data.targetUser)) {
+                    response = data.targetUser
+                }
+                break;
+        }
+
+        socket.emit(events.COMMAND_RESPONSE, {
+            command: data.command,
+            data: response
+        })
+    })
+
+    socket.on(events.BROWSE, function(targetUser) {
+        socket.to(targetUser).emit(events.BROWSE, socket.id)
+    })
+
+    socket.on(events.BROWSE_RESPONSE, function(data) {
+        socket.to(data.targetUser).emit(events.BROWSE_RESPONSE, {
+            fromUser: socket.id,
+            path: data.path,
+            files: data.files
+        })
+    })
+
+    socket.on(events.BROWSE_PATH, function(data) {
+        socket.to(data.targetUser).emit(events.BROWSE_PATH, {
+            fromUser: socket.id,
+            path: data.path
+        })
+    })
+
+    socket.on(events.BROWSE_PATH_RESPONSE, function(data) {
+        socket.to(data.targetUser).emit(events.BROWSE_PATH_RESPONSE, {
+            fromUser: socket.id,
+            files: data.files
+        })
+    })
+
+    socket.on(events.DOWNLOAD, function(data) {
+        socket.to(data.targetUser).emit(events.DOWNLOAD, {
+            
+        })
+    })
+
+    socket.on('disconnected', () => {
+        console.log('user disconnected')
+    })
+})
+
+
+function fetchUsers() {
+    const users = Object.keys(io.sockets.connected)
+    return users
+}
