eae71e57291b0b48a85a0860f6543ff4c901e34d
Christopher Gauffin <christopher.gauffin@gmail.com>
2023-01-24 03:00:24 +0100

    node_modules

--- client_a/test.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-let socket_uri = "http://192.168.1.226:9999";
-console.log("Connecting to %s", socket_uri);
-let client_server = io(socket_uri);
-client_server.on("connect", () => {
-  console.log("Connected to client [%s]", user.id);
-});
